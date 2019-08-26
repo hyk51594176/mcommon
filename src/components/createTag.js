a0603c313c3110def36b0467382e7b23bb5c5a55
hanyukai <51594176@qq.com>
2019-08-26 18:32:59 +0800

    兼容低版本slots

--- src/components/createTag.js
+++ src/components/createTag.js
@@ -1,8 +1,12 @@
 export default function (h, { column, row = {}, $index }) {
   let VNode = null
   const scopedSlots = this.scopedSlots || this.$scopedSlots
-  if (column.prop && scopedSlots[column.prop]) {
+  const slots = this.slots && this.slots()
+
+  if (scopedSlots && column.prop && scopedSlots[column.prop]) {
     VNode = scopedSlots[column.prop]({ row, column, $index })
+  } else if (slots && column.prop && slots[column.prop]) {
+    VNode = slots[column.prop]
   }
   return VNode || h('MItem', {
     props: {
