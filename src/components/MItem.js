f4ca75be93b7854663ee1dad765e0218c4b4acbc
hanyukai <51594176@qq.com>
2019-12-30 17:05:50 +0800

    feat: input空值设置为null

--- src/components/MItem.js
+++ src/components/MItem.js
@@ -10,13 +10,17 @@ const createChildren = (h, el, column, valueKey) => {
       children = item[valueKey.label]
     }
 
-    return h(el, {
-      props: {
-        ...column,
-        label: column.props ? item[column.props.value] : item[valueKey.value]
+    return h(
+      el,
+      {
+        props: {
+          ...column,
+          label: column.props ? item[column.props.value] : item[valueKey.value]
+        },
+        key: item[valueKey.label]
       },
-      key: item[valueKey.label]
-    }, children)
+      children
+    )
   })
 }
 export default {
@@ -32,35 +36,35 @@ export default {
     },
     $index: Number
   },
-  data () {
+  data() {
     return {
       isForce: false
     }
   },
   watch: {
-    row (ov, nv) {
+    row(ov, nv) {
       if (this.$parent.clearValidate && ov !== nv) {
         this.$nextTick(this.$parent.clearValidate)
       }
     }
   },
   computed: {
-    computedColumn () {
+    computedColumn() {
       return {
         ...this.column,
         ...((this.column.getColumn && this.column.getColumn(this.row)) || {})
       }
     },
-    componentType () {
+    componentType() {
       const { el } = this.computedColumn
       if (!el) return null
       if (el === 'mSelect' || el === 'MSelect' || el === 'select' || el === 'el-select') {
         return 'm-select'
       } else {
-        return el.startsWith('el-') ? el : ('el-' + el)
+        return el.startsWith('el-') ? el : 'el-' + el
       }
     },
-    valueKey () {
+    valueKey() {
       if (this.computedColumn.valueKey) return this.computedColumn.valueKey
       return {
         label: 'label',
@@ -68,7 +72,7 @@ export default {
       }
     },
     modelComputed: {
-      get () {
+      get() {
         let val = null
         try {
           val = this.getStrFunction(`this.row.${this.column.prop}`)
@@ -80,17 +84,19 @@ export default {
         }
         return val
       },
-      set (value) {
-        const { type, trim } = this.computedColumn
+      set(value) {
+        const { type, trim, nullString = true } = this.computedColumn
         if (type === 'currency' || type === 'number') {
           const reg = /^-?[0-9]*\.?\d*$/g
           if (!reg.test(value)) {
             value = value.substr(0, value.length - 1)
           }
         }
-        if (trim && this.componentType === 'el-input') {
-          value = value.trim()
+        if (this.componentType === 'el-input') {
+          if (trim) value = value.trim()
+          if (nullString && value === '') value = null
         }
+
         try {
           if (this.getStrFunction(`this.row.${this.computedColumn.prop}`) === undefined) {
             this.setRowKey(value)
@@ -102,12 +108,12 @@ export default {
     }
   },
   methods: {
-    getStrFunction (str) {
+    getStrFunction(str) {
       str = str.replace(/(\.\d)/g, '[$1]').replace(/\.\[/g, '[')
       const Fn = Function
       return new Fn(`return ${str}`).call(this)
     },
-    setRowKey (value) {
+    setRowKey(value) {
       if (this.computedColumn.prop && this.row) {
         const path = this.computedColumn.prop.replace(/\[(\w+)\]/g, '.$1').replace(/^\./, '')
         const arr = path.split('.')
@@ -120,7 +126,7 @@ export default {
               x[y] = value
               return emptyObj
             }
-            if (!x[y])x[y] = {}
+            if (!x[y]) x[y] = {}
             return x[y]
           }, emptyObj)
           this.$set(this.row, firstKey, JSON.parse(JSON.stringify(val)))
@@ -129,28 +135,27 @@ export default {
         }
       }
     },
-    getParams () {
+    getParams() {
       const newObj = {}
       if (this.computedColumn.params && typeof this.computedColumn.params === 'object') {
         for (const key in this.computedColumn.params) {
           let value
           try {
             value = this.getStrFunction(`this.row.${this.computedColumn.params[key]}`)
-          } catch (err) { }
+          } catch (err) {}
           newObj[key] = value !== undefined ? value : this.computedColumn.params[key]
         }
       }
       return newObj
     }
   },
-  render (h) {
+  render(h) {
     const { row, column, $index, computedColumn, modelComputed, componentType, valueKey, getParams } = this
     if (componentType) {
-      const placeholder = computedColumn.placeholder !== undefined
-        ? computedColumn.placeholder : computedColumn.label
+      const placeholder = computedColumn.placeholder !== undefined ? computedColumn.placeholder : computedColumn.label
       const listeners = {
         ...(computedColumn.listeners || {}),
-        input: (val) => {
+        input: val => {
           this.modelComputed = val
           computedColumn.listeners && computedColumn.listeners.input && computedColumn.listeners.input(val)
         }
@@ -164,24 +169,23 @@ export default {
         if (computedColumn.type === 'button') {
           str = '-button'
         }
-        const children = componentType !== 'm-select'
-          ? createChildren(
-            h,
-            componentType === 'el-checkbox-group' ? `el-checkbox${str}` : `el-radio${str}`,
-            computedColumn,
-            valueKey
-          ) : null
-        return h(componentType, {
-          props: {
-            ...computedColumn,
-            placeholder,
-            params: componentType === 'm-select' ? getParams(computedColumn) : null,
-            value: modelComputed,
-            customRender: (computedColumn.slots && computedColumn.slots.default) ? computedColumn.slots.default : null
+        const children =
+          componentType !== 'm-select' ? createChildren(h, componentType === 'el-checkbox-group' ? `el-checkbox${str}` : `el-radio${str}`, computedColumn, valueKey) : null
+        return h(
+          componentType,
+          {
+            props: {
+              ...computedColumn,
+              placeholder,
+              params: componentType === 'm-select' ? getParams(computedColumn) : null,
+              value: modelComputed,
+              customRender: computedColumn.slots && computedColumn.slots.default ? computedColumn.slots.default : null
+            },
+            attrs: componentType === 'm-select' ? computedColumn : null,
+            on: listeners
           },
-          attrs: componentType === 'm-select' ? computedColumn : null,
-          on: listeners
-        }, children)
+          children
+        )
       } else {
         if (componentType === 'el-input' && computedColumn.type === 'currency') {
           listeners.blur = (...args) => {
@@ -200,36 +204,56 @@ export default {
           if (typeof slots[key] === 'function') {
             scopedSlots[key] = slots[key].bind(null, h)
           } else {
-            children.push(h('template', {
-              slot: key
-            }, [slots[key]]))
+            children.push(
+              h(
+                'template',
+                {
+                  slot: key
+                },
+                [slots[key]]
+              )
+            )
           }
         })
-        const attrs = attributes.reduce((obj, key) => {
-          if (this.computedColumn[key]) obj[key] = this.computedColumn[key]
-          return obj
-        }, { placeholder })
-        return h(componentType, {
-          props: {
-            ...computedColumn,
-            filterable: true,
-            value: modelComputed,
-            columnOptions: computedColumn,
-            label: (componentType === 'el-checkbox' || componentType === 'el-radio') ? null : computedColumn.label
+        const attrs = attributes.reduce(
+          (obj, key) => {
+            if (this.computedColumn[key]) obj[key] = this.computedColumn[key]
+            return obj
           },
-          attrs: attrs,
-          scopedSlots,
-          on: listeners
-        }, children)
+          { placeholder }
+        )
+        return h(
+          componentType,
+          {
+            props: {
+              ...computedColumn,
+              filterable: true,
+              value: modelComputed,
+              columnOptions: computedColumn,
+              label: componentType === 'el-checkbox' || componentType === 'el-radio' ? null : computedColumn.label
+            },
+            attrs: attrs,
+            scopedSlots,
+            on: listeners
+          },
+          children
+        )
       }
     } else {
       const VNode = typeof computedColumn.render === 'function' ? computedColumn.render(h, { row, computedColumn, $index }) : computedColumn.render
-      return VNode || h('span', {
-        class: 'item-container',
-        style: {
-          'word-break': 'break-all'
-        }
-      }, computedColumn.format ? computedColumn.format(row) : modelComputed)
+      return (
+        VNode ||
+        h(
+          'span',
+          {
+            class: 'item-container',
+            style: {
+              'word-break': 'break-all'
+            }
+          },
+          computedColumn.format ? computedColumn.format(row) : modelComputed
+        )
+      )
     }
   }
 }
